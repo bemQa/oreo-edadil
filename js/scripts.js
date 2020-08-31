69152999fb7b5726ef83b80da7e4ccb0e39867fc
Alexander K <niktonezapomnil@gmail.com>
2020-08-31 19:41:52 +0500

    full html

--- js/scripts.js
+++ js/scripts.js
@@ -33,74 +33,12 @@ $(document).ready(function () {
         });
     });
 
-    $('.anchor[href^="#"]').click(function () {
-        if($(window).innerWidth() <= 1000) {
-           $('.menu-links').removeClass('active'); 
-           $('.burger').removeClass('active');
-        }
-        elementClick = $(this).attr("href");
-        destination = $(elementClick).offset().top-50;
-        $('html, body').animate( { scrollTop: destination }, 500, 'swing' );
-        return false;
-    });
-
-    $('.profile-link').click(function(e) {
-        e.preventDefault();
-        if($(window).innerWidth() > 1000) {
-            $('.login-menu').fadeToggle('slow');
-        } else {
-            $('.login-menu').toggle();
-        }
-        $('body').on('click', function(e) {
-            var div = $('.menu-links');
-
-            if (!div.is(e.target) && div.has(e.target).length === 0) {
-                if($(window).innerWidth() > 1000) {
-                    $('.login-menu').fadeOut('slow');
-                } else {
-                    $('.login-menu').hide();
-                }
-            }
-        });
-    });
-
-    function OpenPopup(popupId) {
-        $('body').removeClass('no-scrolling');
-        $('.popup').removeClass('js-popup-show');
-        popupId = '#' + popupId;
-        $(popupId).addClass('js-popup-show');
-        $('body').addClass('no-scrolling');
-    }
-    $('.pop-op').click(function (e) {
-        e.preventDefault();
-        let data = $(this).data('popup');
-        OpenPopup(data);
-    });
-    function closePopup() {
-        $('.js-close-popup').on('click', function (e) {
-            e.preventDefault();
-            $('.popup').removeClass('js-popup-show');
-            $('body').removeClass('no-scrolling');
-        });
-    }
-    closePopup();
-    function clickClosePopup(popupId) {
-        popupId = '#' + popupId;
-        $(popupId).removeClass('js-popup-show');
-        $('body').removeClass('no-scrolling');
-    }
-
     $('.table-wrapper').scrollbar();
     $('.faq-wrap').scrollbar();
 
     function maskInit() {
         $(".phone-mask").inputmask({
             mask:"+7(999)999-99-99",
-            "clearIncomplete": true
-        });
-
-        $(".card-mask").inputmask({
-            mask:"9999-9999-9999-9999",
             "clearIncomplete": true
         });
     }
@@ -160,22 +98,6 @@ $(document).ready(function () {
     }
     checkValidate();
 
-    function formatState(opt) {
-        if (!opt.id) {
-            return opt.text;
-        }
-
-        var optimage = $(opt.element).attr('data-image');
-
-        if (!optimage) {
-            return opt.text;
-        } else {
-            var $opt = $(
-                '<span class="select2-image"><div class="select2-image__img"><img src="' + optimage + '"/></div> ' + opt.text + '</span>');
-
-            return $opt;
-        }
-    }
     if($('.select').length > 1) {
         var parent = $('select').parents('.select');
         $('select').each(function() {
@@ -192,42 +114,9 @@ $(document).ready(function () {
             dropdownParent: $('.select')
         });
     }
-    $('.image-select select').each(function() {
-        $(this).select2({
-            templateResult: formatState,
-            templateSelection: formatState,
-            dropdownParent: $(this).closest('.image-select')
-        });
-
-    });
 
     $('select').on('select2:open', function() {
         $('.select2-results__options').scrollbar();
-    });
-
-
-
-    // восстановление пароля
-    $('#restore-password .btn').click(function(e){
-        e.preventDefault();
-        if($('#restore-password form').valid()) {
-            $('#restore-password .btn').addClass('disabled');
-            $('.clock-text, .after-send').show();
-            $('.before-send').hide();
-            let dt = new Date();
-            let time = dt.getFullYear() + '/' + (dt.getMonth()+1) + '/' + dt.getDate() + ' ' + dt.getHours() + ":" + (dt.getMinutes()+1) + ":" + dt.getSeconds();
-            $('.clock').parent().show();
-            $('.clock').countdown(time)
-            .on('update.countdown', function(event) {
-                $(this).html(event.strftime('%M:%S'));
-            })
-            .on('finish.countdown', function(event) {
-                $(this).parent().hide();
-                $('.after-send').hide();
-                $('.before-send').show();
-                $('#restore-password .btn').removeClass('disabled');
-            });
-        }
     });
 
     function openAccordion() {
@@ -252,11 +141,4 @@ $(document).ready(function () {
         });
     }
     openAccordion();
-
-    $('.prize-nav').click(function(e) {
-        e.preventDefault();
-        var data = $(this).data('nav');
-        $('.mob-prizes').hide();
-        $('.'+data).show();
-    });
 });
